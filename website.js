2eebc35d45a511c60fc301cb444aeed6638e8f96
Markuss H <markusshofheins@gmail.com>
2022-02-02 04:22:09 -0800

    Basic website setup

---
+++ website.js
@@ -0,0 +1,10 @@
+import Express from "express";
+
+const app = Express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello World")
+})
+
+app.listen(port, () => console.log('Website Viewed'))
