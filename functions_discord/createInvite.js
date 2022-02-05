00a108dc9d1e7fd2636ec38ff5ad234218d88d73
POPINxxCAPS <markusshofheins@hotmail.com>
2022-02-05 00:28:53 -0700

    More debug

--- functions_discord/createInvite.js
+++ functions_discord/createInvite.js
@@ -1,11 +1,11 @@
- function createInvite(client, guildID) {
+async function createInvite(client, guildID) {
  const guild = client.guilds.cache.get(guildID);
  let invite;
  const channel = guild.channels.cache
    .filter((ch) =>{ return ch.manageable && ch.type === 'text' })
    .first();
- channel.createInvite()
-   .then(inv => { console.log(inv); invite = inv.code });
+ await channel.createInvite()
+   .then(inv => { console.log(inv.code); invite = inv.code });
 console.log(invite)
  return `https://discord.gg/${invite}`
 }
