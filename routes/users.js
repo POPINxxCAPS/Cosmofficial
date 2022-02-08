16e59f35eb628ca8b1a15b8e6acd83d91030fc52
Markuss H <markusshofheins@gmail.com>
2022-02-08 00:22:21 -0700

    URL Route for single player set up

--- routes/users.js
+++ routes/users.js
@@ -3,23 +3,40 @@ const router = express.Router();
 const statusModel = require('../models/statusSchema');
 const playerModel = require('../models/playerSchema');
 const playerEcoModel = require('../models/playerEcoSchema');
+const verificationModel = require('../models/verificationSchema');
 
 // Get one server
 router.get('/:guildID', getGuildID, async (req, res) => {
     const server = await statusModel.findOne({
         guildID: req.params.guildID
     })
+    // If there is a username mentioned, show the single user page
+    if(req.query.username !== undefined) {
+        const user = await playerModel.findOne({
+            guildID: req.params.guildID,
+            displayName: req.query.username
+        })
+        if(user === null) {
+            return res.status(404).json({ message: 'Username was not found'})
+        }
+        const data = {
+            server: server,
+            user: user
+        }
 
-    const playerDocs = await playerModel.find({
-        guildID: req.params.guildID
-    })
-    const users = playerDocs.sort((a, b) => ((a.lastLogin) > (b.lastLogin)) ? -1 : 1)
-    const data = {
-        server: server,
-        users: users
+        res.render("singleServerUser.ejs", data)
+    } else { // If no username mentioned, show all users
+        const playerDocs = await playerModel.find({
+            guildID: req.params.guildID
+        })
+        const users = playerDocs.sort((a, b) => ((a.lastLogin) > (b.lastLogin)) ? -1 : 1)
+        const data = {
+            server: server,
+            users: users
+        }
+    
+        res.render("singleServerUsers.ejs", data)
     }
-
-    res.render("singleServerUsers.ejs", data)
 })
 
 async function getGuildID(req, res, next) {
