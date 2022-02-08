f1992353a63b181720361b973cc73db60bb99dd3
Markuss H <markusshofheins@gmail.com>
2022-02-08 02:12:15 -0700

    Nevermind, login system is fine. Just accessed it wrong

--- functions_oAuth/getUser.js
+++ functions_oAuth/getUser.js
@@ -1,13 +1,7 @@
-const DiscordOauth2 = require("discord-oauth2-api");
-const oauth = new DiscordOauth2({
-	clientId: "845784381025157131",
-	clientSecret: process.env.oauthClientSecret || require('../env/env').oauthClientSecret,
-	redirectUri: "http://localhost/login",
-});
+const oauth = require("../oAuth")
 
-async function getUser(access_token) {
-    let user = await oauth.getUser(access_token)
-    return user;
+function getUser(access_token) {
+    oauth.getUser(access_token).then(console.log);
 }
 
 module.exports = getUser
--- functions_oAuth/verifyKey.js
+++ functions_oAuth/verifyKey.js
@@ -3,8 +3,9 @@ const client = require('../oAuth');
 const verify = async (req, res, next) => {
     if (req.cookies['doaKey'] !== undefined && req.cookies['doaKey'] !== 'deleted') {
         try {
-            const validity = client.checkValidity(req.cookies['doaKey']);
-            if (validity.expired) {
+            const validity = await client.checkValidity(req.cookies['doaKey']);
+            console.log(validity)
+            if (validity.expired === true) {
                 const newKey = await client.refreshToken(req.cookies['doaKey']);
                 res.cookie('doaKey', newKey);
             }
