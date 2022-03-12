a29c4db1f30e2228f8fc3c53d0b1aa0d6bfb1950
Markuss H <markusshofheins@gmail.com>
2022-03-12 03:50:57 -0700

    Random future-common code

---
+++ functions_misc/makeConfigVar.js
@@ -0,0 +1,19 @@
+const getAllSettings = require("../functions_db/getAllSettings");
+
+module.exports = async (guildID, settings) => {
+    if(settings === undefined) {
+        settings = await getAllSettings(guildID);
+    }
+    const remote = settings.find(set => set.name === 'remote')
+    let config = {};
+    try { // .find() Causes crashes, so I need to do it this way.
+        config.ip = remote.settings.find(set => set.setting === 'ip').value;
+        config.port = remote.settings.find(set => set.setting === 'port').value;
+        config.secret = remote.settings.find(set => set.setting === 'secret').value;
+        config.prefix = '/vrageremote';
+    } catch (err) {
+        console.log(`There was an error making the config var for guildID ${guildID}.`)
+        return null;
+    }
+    return config;
+}
