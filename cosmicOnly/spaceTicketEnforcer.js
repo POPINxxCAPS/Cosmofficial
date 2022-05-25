aa32a44ec3936b9c26db5a74fe27f3c5e209ce81
Markuss H <markusshofheins@gmail.com>
2022-05-25 09:35:55 -0600

    Ensured boundaries check for space tickets (cosmic)

--- cosmicOnly/spaceTicketEnforcer.js
+++ cosmicOnly/spaceTicketEnforcer.js
@@ -37,12 +37,13 @@ module.exports = (client) => {
         }
 
         for (let grid of gridDocs) {
-            if(allowedFactionTags.includes(grid.factionTag) === true) continue;
             let index = gridDocs.indexOf(grid);
             if (NPCNames.includes(grid.ownerDisplayName) === true || grid.ownerDisplayName.includes(" CEO") === true) continue;
             if (grid.queuedForDeletion === true && grid.deletionReason !== 'left permitted boundaries') continue;
             let inBoundary = false;
+            if(allowedFactionTags.includes(grid.factionTag) === true) inBoundary = true;
             for (const buoy of buoyDocs) { // Check if grid is within a boundary buoy
+                if(inBoundary === true) continue;
                 let cutString = buoy.displayName.replace('Lane Buoy', '');
                 let radius = cutString === '' ? 50000 : parseInt(cutString);
 
@@ -53,14 +54,16 @@ module.exports = (client) => {
                 let distance = Math.sqrt(dx * dx + dy * dy + dz * dz);
                 if (distance < radius) {
                     inBoundary = true;
+                    //console.log(`${grid.displayName} In Boundary: ${inBoundary}`)
                 }
             }
 
-            if (inBoundary === false && grid.queuedForDeletion === false) { // Check if there is a space ticket, if not do grid deletion
+            if (inBoundary === false && grid.queuedForDeletion === false && allowedFactionTags.includes(grid.factionTag) === false) { 
+                // Check if there is a space ticket, if not do grid deletion
                 // Grid Deletion Marking Stuff
                 grid.deletionReason = 'left permitted boundaries'
                 grid.queuedForDeletion = true;
-                grid.deletionTime = current_time + 150000;
+                grid.deletionTime = current_time + 300000;
                 grid.save().then(savedDoc => {
                     gridDocs[index] = savedDoc;
                 })
@@ -70,10 +73,11 @@ module.exports = (client) => {
                 if (verDoc !== null) {
                     let memberTarget = await guild.members.cache.find(member => member.id === verDoc.userID)
                     try {
-                        memberTarget.send(`**__Warning__**\n>>> ${grid.displayName} has left the Cosmic Boundary.\nBuy a Space Ticket with c!bst or re-enter the zones.\nOtherwise, grid will be removed in ~2-5 minutes.`)
+                        memberTarget.send(`**__Warning__**\n>>> ${grid.displayName} has left the Cosmic Boundary.\nBuy a Space Ticket with c!bst or re-enter the zones.\nOtherwise, grid will be removed in ~5 minutes.`)
                     } catch (err) {}
                 }
             } else if(grid.queuedForDeletion === true && (inBoundary === true || allowedFactionTags.includes(grid.factionTag === true)) === true) {
+                // Remove queue status from grids that re-enter or buy a space ticket
                 grid.deletionReason = '';
                 grid.queuedForDeletion = false;
                 grid.save().then(savedDoc => {
