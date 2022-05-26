2964fe925795ddef108aef901326c5199c8d72f2
Markuss H <markusshofheins@gmail.com>
2022-05-26 03:52:37 -0600

    Added loot box spawners to name list

--- handlers/spawnerGridHandler.js
+++ handlers/spawnerGridHandler.js
@@ -1,12 +1,11 @@
-const spawnerGridNames = ['Zone Chip Spawner', 'Ice Spawner', 'Iron Spawner', 'Silicon Spawner', 'Cobalt Spawner', 'Silver Spawner', 'Magnesium Spawner', 'Gold Spawner', 'Platinum Spawner', 'Uranium Spawner', 'Powerkit Spawner', 'Space Credit Converter']
+const spawnerGridNames = ['Zone Chip Spawner', 'Ice Spawner', 'Iron Spawner', 'Silicon Spawner', 'Cobalt Spawner', 'Silver Spawner', 'Magnesium Spawner', 'Gold Spawner', 'Platinum Spawner', 'Uranium Spawner', 'Powerkit Spawner', 'Space Credit Converter', 'Common Loot Box', 'Ramshackle Loot Box', 'Apprentice Loot Box', 'Journeyman Loot Box', 'Mastercraft Loot Box', 'Ascendant Loot Box']
 const spawnerModel = require('../models/spawnerSchema');
 
 module.exports = async (guildID, grid) => {
     const current_time = Date.now();
     if (spawnerGridNames.includes(grid.DisplayName) === false || grid.OwnerDisplayName !== "Space Pirates") return;
-
     // If it's a spawner grid, check to see if the grid should be powered off. (Deactivation timer failed due to crash or server restart)
-    let spawnerDoc = await spawnerModel.findOne({
+    const spawnerDoc = await spawnerModel.findOne({
         guildID: guildID,
         gridName: grid.DisplayName
     })
