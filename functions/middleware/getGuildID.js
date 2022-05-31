bfdecbcdb2e4358f8fd871cb74ed44453bd9de79
Markuss H <markusshofheins@gmail.com>
2022-05-31 01:24:20 -0600

    Fixed select server button

--- functions/middleware/getGuildID.js
+++ functions/middleware/getGuildID.js
@@ -1,17 +1,17 @@
+const statusModel = require('../../models/statusSchema');
 async function getGuildID(req, res, next) {
     let server;
- try {
-    server = await statusModel.findOne({
-        guildID: req.params.guildID
-    })
-    if(server === null) {
-        return res.status(404).json({ message: 'GuildID was not found'})
+    try {
+        server = await statusModel.findOne({
+            guildID: req.params.guildID
+        })
+    } catch (err) {
+        return res.status(500).json({
+            message: err.message
+        })
     }
- } catch (err) {
-     return res.status(500).json({ message: err.message })
- }
- res.server = server
- next()
+    res.server = server
+    next()
 }
 
 module.exports = getGuildID;
--- routes/servers.js
+++ routes/servers.js
@@ -6,14 +6,16 @@ const verifyKey = require('../functions_oAuth/verifyKey');
 const getUser = require('../functions_oAuth/getUser');
 const getGuildID = require('../functions/middleware/getGuildID');
 
-// Get full server list
 
+// Get full server list
 router.get('/', verifyKey, async (req, res) => {
     const reqObj = {}; // Object for sending information to the page in one var
     reqObj.authURL = res.authURL;
     if(reqObj.authURL === 'Logged-In') {
         reqObj.user = await getUser(req.cookies['doaKey'])
     }
+
+    // Server list page
     reqObj.servers = await statusModel.find({
         serverName: { $ne: "Connection Error" },
         serverOnline: true
@@ -23,15 +25,16 @@ router.get('/', verifyKey, async (req, res) => {
 })
 
 // Get one server
-router.get('/:guildID', getGuildID, verifyKey, async (req, res) => {
+router.get('/single/:guildID', getGuildID, verifyKey, async (req, res) => {
     let count;
     await gridCount(req.params.guildID).then(result => { count = result });
-
-    const server = await statusModel.findOne({
-        guildID: req.params.guildID
-    })
+    const reqObj = {}; // Object for sending information to the page in one var
+    reqObj.authURL = res.authURL;
+    if(reqObj.authURL === 'Logged-In') {
+        reqObj.user = await getUser(req.cookies['doaKey'])
+    }
     res.render("singleServer.ejs", {
-        server: server,
+        server: res.server,
         stations: count.stations,
         ships: count.ships,
         NPCs: count.NPCs
